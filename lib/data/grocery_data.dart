61171fc95b5637bd4d6960798d53a854d9af03b7
ROHANUR RAHMAN <mdrohanurrahmanontu@gmail.com>
2024-02-27 14:17:23 +0600

    project structured

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,35 @@
+class Gorcerydata{
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      'name': 'Apples',
+      'description': 'Fresh and juicy apples',
+      'price': 2.99,
+      'imageUrl': 'https://images.pexels.com/photos/1510392/pexels-photo-1510392.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      'name': 'Bananas',
+      'description': 'Ripe bananas, perfect for a snack',
+      'price': 1.99,
+      'imageUrl': 'https://images.pexels.com/photos/2875814/pexels-photo-2875814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      'name': 'Milk',
+      'description': 'Fresh dairy milk',
+      'price': 3.49,
+      'imageUrl': 'https://images.pexels.com/photos/5836625/pexels-photo-5836625.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      'name': 'Bread',
+      'description': 'Whole wheat bread',
+      'price': 2.00,
+      'imageUrl': 'https://images.pexels.com/photos/209206/pexels-photo-209206.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      'name': 'Eggs',
+      'description': 'Farm-fresh eggs',
+      'price': 2.50,
+      'imageUrl': 'https://images.pexels.com/photos/162712/egg-white-food-protein-162712.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+  ];
+
+}
---
+++ lib/features/home/ui/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+
+}
+
+class  HomeState extends State<Home>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+}
